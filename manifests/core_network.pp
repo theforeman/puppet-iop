fe98fe3ddd67823998011a00f330673334461b99
Eric D. Helms <ericdhelms@gmail.com>
2025-05-01 13:54:06 -0400

    Add kafka spec tests and idempotent init
    Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

---
+++ manifests/core_network.pp
@@ -0,0 +1,19 @@
+# == Class: iop::core_network
+#
+# Configure podman network for services
+#
+# === Parameters:
+#
+# $ensure:: Ensure service is present or absent
+#
+class iop::core_network (
+  Enum['present', 'absent'] $ensure = 'present',
+) {
+  include podman
+
+  podman::network { 'iop-core-network':
+    ensure => $ensure,
+    driver => 'bridge',
+    subnet => '10.130.0.0/24',
+  }
+}
