ed04fc230d1443b1e7af1147a396c36d7b384439
Eric D. Helms <ericdhelms@gmail.com>
2025-07-18 09:06:35 -0400

    Use curl directly for download handling
    Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

--- manifests/metadata_downloader.pp
+++ manifests/metadata_downloader.pp
@@ -1,47 +1,26 @@
 # @summary Manages IoP metadata download
 #
+# @param ensure
+#   'present' to create and enable the service/timer,
+#   'absent' to remove and disable them.
+#   Defaults to 'present'.
 #
-# @param ensure: 'present' to create and enable the service/timer,
-#                'absent' to remove and disable them.
-#                Defaults to 'present'.
-#
-# @param base_url: Base URL for the metadata
+# @param base_url
+#   Base URL for the metadata file to download.
+#   Defaults to the Red Hat security CVE map XML file.
 #
 class iop::metadata_downloader (
   Enum['present', 'absent'] $ensure = 'present',
-  String $base_url = 'https://security.access.redhat.com/data/'
+  String $base_url = 'https://security.access.redhat.com/data/meta/v1/cvemap.xml',
 ) {
+  $basedir = '/var/www/html/pub'
+  $path = dirname($base_url)
+  $filename = 'cvemap.xml'
+  $full_path = "${basedir}/${path}/${filename}"
 
-  # Define paths and names for the script, service, and timer
-  $script_path = '/usr/local/bin/iop-metadata-download.sh'
-  $service_name = 'iop-metadata-download-cvemap.service'
-  $timer_name = 'iop-metadata-download-cvemap.timer'
-
-  $basedir = '/var/www/html/pub/iop/data'
-  $cvemap_base = 'meta/v1/cvemap.xml'
-  $cvemap_path = "$basedir/$cvemap_base"
-
-  file { dirname($cvemap_path):
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    recurse => true
-  }
-
-
-  file { $script_path:
-    ensure  => $ensure,
-    path    => $script_path,
-    content => file('iop/iop-metadata-download.sh')
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-  }
-
-  systemd::timer_wrapper {
+  systemd::timer_wrapper { 'iop-metadata-download-cvemap':
     ensure                 => $ensure,
-    command                => [$script_path, "${base_url}/$cvemap_base", $cvemap_path]
+    command                => "/usr/bin/curl --create-dirs --fail --silent --remote-time --time-cond ${full_path} --output ${full_path} ${base_url}",
     on_boot_sec            => '1h',
     on_unit_active_sec     => '4h',
     service_unit_overrides => { 'Wants' => 'network-online.target' },
