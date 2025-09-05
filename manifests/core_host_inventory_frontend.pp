649db95896889bee767757fe09f0867e7e1e6c5b
Eric D. Helms <ericdhelms@gmail.com>
2025-09-05 08:09:38 -0400

    Add registry auth environment handling for frontend images

--- manifests/core_host_inventory_frontend.pp
+++ manifests/core_host_inventory_frontend.pp
@@ -12,10 +12,18 @@ class iop::core_host_inventory_frontend (
   String[1] $image = 'quay.io/iop/host-inventory-frontend:latest',
   Enum['present', 'absent'] $ensure = 'present',
 ) {
+  include podman
   ensure_resource('file', '/var/lib/foreman/public/assets/apps', { 'ensure' => 'directory' })
 
+  podman::image { 'core_host_inventory_frontend':
+    ensure   => $ensure,
+    image    => $image,
+    exec_env => ['REGISTRY_AUTH_FILE=/etc/foreman/registry-auth.json'],
+  }
+
   iop_frontend { '/var/lib/foreman/public/assets/apps/inventory':
-    ensure => $ensure,
-    image  => $image,
+    ensure  => $ensure,
+    image   => $image,
+    require => Podman::Image['core_host_inventory_frontend'],
   }
 }
--- manifests/service_advisor_frontend.pp
+++ manifests/service_advisor_frontend.pp
@@ -12,10 +12,18 @@ class iop::service_advisor_frontend (
   String[1] $image = 'quay.io/iop/advisor-frontend:latest',
   Enum['present', 'absent'] $ensure = 'present',
 ) {
+  include podman
   ensure_resource('file', '/var/lib/foreman/public/assets/apps', { 'ensure' => 'directory' })
 
+  podman::image { 'service_advisor_frontend':
+    ensure   => $ensure,
+    image    => $image,
+    exec_env => ['REGISTRY_AUTH_FILE=/etc/foreman/registry-auth.json'],
+  }
+
   iop_frontend { '/var/lib/foreman/public/assets/apps/advisor':
-    ensure => $ensure,
-    image  => $image,
+    ensure  => $ensure,
+    image   => $image,
+    require => Podman::Image['service_advisor_frontend'],
   }
 }
