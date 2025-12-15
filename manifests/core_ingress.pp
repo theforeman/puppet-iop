6a546a0ec3506546467e49cea8827f72393bc4db
dkuc <9499094+dkuc@users.noreply.github.com>
2025-12-15 09:58:24 -0600

    Automatically cleanup insights-archives after 24 hours

--- manifests/core_ingress.pp
+++ manifests/core_ingress.pp
@@ -16,11 +16,27 @@ class iop::core_ingress (
   include iop::core_kafka
   include iop::core_network
 
+  file { '/var/tmp/insights-archives':
+    ensure => directory,
+    owner  => '1001',
+    group  => '1001',
+    mode   => '0755',
+  }
+
+  file { '/etc/tmpfiles.d/insights-archives-cleanup.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "e /var/tmp/insights-archives - - - 24h\n",
+  }
+
   podman::quadlet { 'iop-core-ingress':
     ensure       => $ensure,
     quadlet_type => 'container',
     user         => 'root',
     defaults     => {},
+    require      => File['/var/tmp/insights-archives'],
     settings     => {
       'Unit'      => {
         'Description' => 'IOP Core Ingress Container',
@@ -29,11 +45,12 @@ class iop::core_ingress (
         'Image'         => $image,
         'ContainerName' => 'iop-core-ingress',
         'Network'       => 'iop-core-network',
+        'Volume'        => '/var/tmp/insights-archives:/var/tmp/insights-archives:rw,Z',
         'Environment'   => [
           'INGRESS_VALID_UPLOAD_TYPES=advisor,compliance,qpc,rhv,tower,leapp-reporting,xavier,playbook,playbook-sat,malware-detection,tasks',
           'INGRESS_KAFKA_BROKERS=iop-core-kafka:9092',
           'INGRESS_STAGERIMPLEMENTATION=filebased',
-          'INGRESS_STORAGEFILESYSTEMPATH=/var/tmp',
+          'INGRESS_STORAGEFILESYSTEMPATH=/var/tmp/insights-archives',
           'INGRESS_SERVICEBASEURL=http://iop-core-ingress:8080',
           'INGRESS_WEBPORT=8080',
           'INGRESS_METRICSPORT=3001',
